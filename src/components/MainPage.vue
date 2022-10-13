555a1d17600305f1c683ce7137e96b54f3418ff9
Shinya Tanaka <tanakashinya@team-lab.com>
2022-10-13 19:55:35 +0900

    feat: Bootstrap,FA,Sassを追加

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="main-page">メインページです</div>
+  <div class="main-page">
+    <div class="left-menu">左メニュー</div>
+    <div class="right-view">右ビュー</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .main-page {
-  padding: 10px;
+  display: flex;
+  height: calc(100vh - 60px);
+  .left-menu {
+    width: 350px;
+    background-color: #f7f6f3;
+  }
+  .right-view {
+    flex-grow: 1;
+    padding: 10px;
+  }
 }
 </style>
